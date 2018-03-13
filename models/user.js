8d8c2c5ae24247fc1aa033718ff6b47e02ef89d2
Srishti Sehtia <srishti0123@gmail.com>
2018-03-13 15:28:11 -0700

    define user model and schema

---
+++ models/user.js
@@ -0,0 +1,8 @@
+var mongoose = require('mongoose'),
+    Schema = mongoose.Schema,
+    passportLocalMongoose = require('passport-local-mongoose');
+
+var UserSchema = new Schema({
+  username: String,
+  password: String
+});
